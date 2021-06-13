950df2bb0acac0fd1df11ae38c1d9f14959d3b1f
Yusuke Kato <s1526035cn@s.chibakoudai.jp>
2021-06-13 18:08:10 +0900

    image_search

---
+++ scripts/image_search.py
@@ -0,0 +1,55 @@
+#/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import json
+import keys
+from requests_oauthlib import OAuth1Session
+
+class Search():
+    def image_search(self):
+
+        #API認証情報
+        CK = keys.CK
+        CS = keys.CS
+        AT = keys.AT
+        ATS = keys.ATS
+        twitter = OAuth1Session(CK,CS,AT,ATS)
+
+        #API取得
+        search = "https://api.twitter.com/1.1/search/tweets.json"
+
+        # 画像を保存するディレクトリ
+        folder_path = 'folder_path'
+        path = os.path.exists(folder_path)
+        if not path:
+            os.mkdir(folder_path)
+
+        # ツイートの取得設定
+        num = 1
+        keyword = "#kuwamai"
+        params = {'q' : keyword, 'count' : num}
+        req = twitter.get(search, params = params)
+
+        if req.status_code == 200:
+            # ツイートの画像を取得
+            tweets = json.loads(req.content)
+            for tweet in tweets['statuses']:
+                if 'media' in tweet['entities']:
+                    urls = tweet['entities']['media']
+                    media_urls = urls[0]['media_url']
+                    downloads = twitter.get(media_urls).content
+                    # 画像を保存
+                    folder_path = 'folder_path'
+                    file_name = 'file_name.jpg'
+                    file_path = folder_path + '/' + file_name
+                    images = open(file_path, 'wb')
+                    images.write(downloads)
+                    images.close()
+
+        else:
+            print "ERROR: %d" % req.status_code
+
+if __name__ == '__main__':
+    Search().image_search()
